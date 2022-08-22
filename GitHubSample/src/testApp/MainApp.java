ee1ee1f40a909e0be40164508909b4008a06849c
kaychen <grasekao@gmail.com>
2022-08-22 19:39:09 +0800

    Math Branch - Add Math()

--- GitHubSample/src/testApp/MainApp.java
+++ GitHubSample/src/testApp/MainApp.java
@@ -4,7 +4,15 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.print("點火儀式 2nd version");
-
+		// 這是增加數學運算的版本
+		Math cMath = new Math();
+		System.out.println();
+		System.out.println(cMath.add(10, 5));
 	}
 
+}
+class Math{
+	int add(int a, int b) {
+		return a+b;
+	}
 }
